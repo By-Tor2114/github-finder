b313319664a6e70e8f8a3447e789ff297fb00ab1
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-02 11:40:14 +0000

    adds the ability to search for users from the github api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-import { getUsers } from './utils/getUsers';
+import { getUsers, searchUsers } from './utils/users';
 import Search from './components/users/Search';
 
 class App extends Component {
@@ -16,13 +16,19 @@ class App extends Component {
     this.setState({ users: data, isLoading: false });
   };
 
+  // Searches users on Github
+  searchUsers = async text => {
+    const { items } = await searchUsers(text);
+    this.setState({ users: items, isLoading: false });
+  };
+
   render() {
     const { isLoading, users } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users isLoading={isLoading} users={users} />
         </div>
       </div>
---
+++ src/utils/users.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const getUsers = async () => {
+  const users = await axios.get('https://api.github.com/users', {
+    params: {
+      client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+      client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET
+    }
+  });
+  return users;
+};
+
+const searchUsers = async text => {
+  console.log(text, 'in function');
+
+  const users = await axios.get('https://api.github.com/search/users', {
+    params: {
+      q: text,
+      client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+      client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET
+    }
+  });
+  return users.data;
+};
+
+export { getUsers, searchUsers };
