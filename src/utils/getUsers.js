a8e0872d66f600af2ee69308dfc95429b25cf2e0
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-02 10:58:23 +0000

    adds api key to axios requests

--- src/utils/getUsers.js
+++ src/utils/getUsers.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const getUsers = async () => {
-  const users = await axios.get('https://api.github.com/users');
+  const users = await axios.get('https://api.github.com/users', {
+    params: {
+      client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+      client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET
+    }
+  });
   return users;
 };
 
