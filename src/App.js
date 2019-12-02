fde406f32daa6775c7d9559bbfa1207b38a29f94
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-02 07:47:43 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const name = '';
+
+    return (
+      <div className="App">
+        <h1>Testing React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
